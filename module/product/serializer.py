65553b0d5efb6bdaa228823398e4a3c3b1e325cb
Isael Diroche <isaeldiroche00@gmail.com>
2025-01-31 22:04:24 -0400

    Update: Removed product images comments

--- module/product/serializer.py
+++ module/product/serializer.py
@@ -4,13 +4,7 @@ from .models import Product
 # Create your serializers here.
 
 class ProductSerializer(serializers.ModelSerializer):
-    # image_url = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
     
-    # def get_image_url(self, obj):
-    #     request = self.context.get('request')
-    #     if obj.image and request:
-    #         return request.build_absolute_uri(obj.image.url)
-    #     return None
